a4074c3e616a3a0a196d6fa0b9376ac6b532ef8a
Kaare Hoff Skovgaard <khs@secoya.dk>
2016-07-28 12:35:59 +0200

    Reorganized the text nodes to align with expression nodes

--- src/trees/text.js
+++ src/trees/text.js
@@ -4,14 +4,19 @@
 
 import type {
 	Node as ExpressionNode,
+	TypedNode as TypedExpressionNode,
 } from './expression';
+
+import type {
+	InferredType,
+} from '../type_map';
 
 export type Pos = {
 	firstLine: number,
 	firstColumn: number,
 	lastLine: number,
 	lastColumn: number,
- };
+};
 
 export type LiteralNode = {
 	textNodeType: 'literal',
@@ -19,10 +24,24 @@ export type LiteralNode = {
 	pos: Pos,
 };
 
+export type TypedLiteralNode = {
+	textNodeType: 'literal',
+	value: string,
+	pos: Pos,
+	textType: InferredType,
+}
+
 export type VariableNode = {
 	textNodeType: 'variable',
 	value: string,
 	pos: Pos,
+};
+
+export type TypedVariableNode = {
+	textNodeType: 'variable',
+	value: string,
+	pos: Pos,
+	textType: InferredType,
 };
 
 export type ExprNode = {
@@ -31,8 +50,21 @@ export type ExprNode = {
 	pos: Pos,
 };
 
+export type TypedExprNode = {
+	textNodeType: 'expr',
+	value: TypedExpressionNode,
+	pos: Pos,
+	textType: InferredType,
+};
+
 export type Node =
 	| LiteralNode
 	| VariableNode
 	| ExprNode
+;
+
+export type TypedNode =
+	| TypedLiteralNode
+	| TypedVariableNode
+	| TypedExprNode
 ;
