1fd14026475af99a69adce85833db57fc2b55ca5
Kaare Hoff Skovgaard <khs@secoya.dk>
2016-09-08 14:21:57 +0200

    More fixed for TypeScript 2.0 RC

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="./ast-types.d.ts" />
-/// <reference path="./jison.d.ts" />
-/// <reference path="./recast.d.ts" />
-
-declare var process: any;
-declare var __dirname: string;
-declare var require: (path: string) => any;
---
+++ typings/node.d.ts
@@ -0,0 +1,5 @@
+declare const __dirname: string;
+declare function require(path: string): any;
+declare const process: {
+	env: any,
+};
